67f1658273fb24019e3392837a47a00cf5d37416
oscar218 <oscar_m06@hotmail.fr>
2020-11-01 08:48:45 +0000

    background-color: #2ecc71; login_php

--- login.php
+++ login.php
@@ -95,8 +95,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <title>Login</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
     <style type="text/css">
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
+        body{ 
+		font: 14px sans-serif;
+		background-color:#2ecc71;}
+        .wrapper{ width: 350px;
+		padding: 20px; }
     </style>
 </head>
 <body>
